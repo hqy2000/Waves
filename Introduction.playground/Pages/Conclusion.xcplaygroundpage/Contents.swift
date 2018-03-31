168dc1aad598438417d1b51b919bad62b2e4ca51
胡清阳 <huqingyang2000@gmail.com>
2018-03-31 19:06:07 +0800

    Tidy up

--- Introduction.playground/Pages/Conclusion.xcplaygroundpage/Contents.swift
+++
@@ -1,5 +0,0 @@
-/*:
- Now we have stepped through all the theories and equipments required for our experiments. In the next chapter, you will expore some more fun characteristics about waves through interactive contents.
- 
- [Next](@next)
- */
